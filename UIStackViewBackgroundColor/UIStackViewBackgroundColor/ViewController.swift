5e2a4690ee9d9f079fc32c16f063d01cc47a7b23
Daichi Hayashi <daichikuwa0618@gmail.com>
2020-10-20 20:45:23 +0900

    add: VStack の中に HStack が 3 つあるような VC を作成 (各 StackView には背景色を付与している)

---
+++ UIStackViewBackgroundColor/UIStackViewBackgroundColor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIStackViewBackgroundColor
+//
+//  Created by 林 大地 on 2020/10/20.
+//  Copyright © 2020 林 大地. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
